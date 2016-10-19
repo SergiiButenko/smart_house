31b14b3ad940a92be81239e1cf608ffa2b1ca40c
Sergii Butenko <sergii.butenko@globallogic.com>
2016-10-19 15:00:15 +0300

    Removed files and modified gitwebhook php

--- github-webhook/index.php
+++ github-webhook/index.php
@@ -1,6 +1,7 @@
 <?php
 // Use in the "Post-Receive URLs" section of your GitHub repo.
 if ( $_POST['payload'] ) {
+  echo 'Executed: '
   shell_exec('sh ~/conditioner_update_frontend.sh');
 }
-?>hi
+?>Done!
