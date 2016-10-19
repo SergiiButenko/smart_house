7018f5b161cf90ec206fe18b15373a725a94f31b
Sergii Butenko <sergii.butenko@globallogic.com>
2016-10-19 18:28:50 +0300

    Changed shell script

--- github-webhook/index.php
+++ github-webhook/index.php
@@ -2,7 +2,7 @@
 try{
     // Use in the "Post-Receive URLs" section of your GitHub repo.
     if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-      shell_exec('~/git-hook/conditioner_update_frontend.sh >> ~/git-hook/logs/"$(date +%F_%R)_log"');
+      shell_exec('. ~/git-hook/conditioner_update_frontend.sh >> ~/git-hook/logs/"$(date +%F_%R)_log"');
       echo 'Updated!';
     } else {
         echo "Works fine.";
