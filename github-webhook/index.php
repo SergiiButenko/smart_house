1afc4742a97a6974f0f9d84901fc47e77f9de505
Sergii Butenko <sergii.butenko@globallogic.com>
2016-10-19 15:56:05 +0300

    test

--- github-webhook/index.php
+++ github-webhook/index.php
@@ -2,6 +2,6 @@
 // Use in the "Post-Receive URLs" section of your GitHub repo.
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
   echo 'Executed: ';
-  shell_exec('sh ~/conditioner_update_frontend.sh >> ~/($date +'%Y-%m-%d')_log');
+  shell_exec('sh ~/conditioner_update_frontend.sh >> ~/($date +'%Y-%m-%d')_log');`
 }
-?>Done!
+?>Done! 
