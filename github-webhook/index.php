7a862a7fae6b4c9aefe5742a16a84d429c596179
Sergii Butenko <sergii.butenko@globallogic.com>
2016-10-19 18:05:40 +0300

    Changed location of update script

--- github-webhook/index.php
+++ github-webhook/index.php
@@ -1,7 +1,13 @@
 <?php
-// Use in the "Post-Receive URLs" section of your GitHub repo.
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-  echo 'Executed: ';
-  shell_exec('~/conditioner_update_frontend.sh >> ~/"$(date +%F_%R)_log"');
-}
-?>Done! 
+try{
+    // Use in the "Post-Receive URLs" section of your GitHub repo.
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+      shell_exec('~/git-hook/conditioner_update_frontend.sh >> ~/git-hook/logs/"$(date +%F_%R)_log"');
+      echo 'Updated!';
+    } else {
+        echo "Works fine.";
+    }
+} catch ( Exception $e ) {
+    echo $e->getMessage();
+  }
+?>
