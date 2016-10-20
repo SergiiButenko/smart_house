f3dedfd48dc86f4019c7297d859a38ca0a7f5e68
Sergii Butenko <butenko2003@ukr.net>
2016-10-20 20:37:25 +0300

    Last fix

--- github-webhook/index.php
+++ github-webhook/index.php
@@ -2,7 +2,7 @@
 try{
     // Use in the "Post-Receive URLs" section of your GitHub repo.
     if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-      shell_exec('sh /ffp/home/root/git-hook/conditioner_update_frontend.sh >> ~/git-hook/logs/"$(date +%F_%R)_log" 2>&1');
+      shell_exec('sh /ffp/home/root/git-hook/conditioner_update_frontend.sh >> /ffp/home/root/git-hook/logs/"$(date +%F_%R)_log" 2>&1');
       echo 'Updated!';
     } else {
         echo "Works fine.";
