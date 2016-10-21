d27167bb4d5f72c37c6f9f0eb3606f7febbccea1
Sergii Butenko <sergii.butenko@globallogic.com>
2016-10-21 16:56:59 +0300

    Adde php script file

--- index.php
+++ index.php
@@ -5,10 +5,19 @@
 <body>
 <?php 
  require 'php/db.php';
+ require 'php/check_mobile.php'
+ 
  foreach (statuses() as $key => $value) {
   echo $key . ": status: " . $value['status'] . "; settings: " . $value['settings'];
   echo "<br>";
-}
+ }
+  
+  echo "<br>";
+  if (is_mobile()){
+      echo 'It is mobile'; 
+    } else {
+      echo 'No mobile browser detected.';
+    } 
 ?>
 </body>
 </html>
