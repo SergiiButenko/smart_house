a1036b5f035debf9a87080eec0e286099f17f73e
Sergii Butenko <sergii.butenko@globallogic.com>
2016-10-21 16:20:39 +0300

    Changed main php file

--- index.php
+++ index.php
@@ -1,25 +1,14 @@
 <html>
 <head>
-<title>Retrieve data from database </title>
+<title>Retrieve data from database</title>
 </head>
 <body>
-<?php
-// Connect to database server 
-mysql_connect("192.168.1.104:3306", "php_user", "password") or die (mysql_error());
-// Select database
-mysql_select_db("test") or die(mysql_error());
-// SQL query
-$strSQL = "SELECT * FROM conditioners";
-// Execute the query (the recordset $rs contains the result)
-$rs = mysql_query($strSQL);
-// Loop the recordset $rs
-// Each row will be made into an array ($row) using mysql_fetch_array
-while($row = mysql_fetch_array($rs)) {
-// Write the value of the column FirstName (which is now in the array $row)
-echo $row['name'] . ":" . $row['status'] . "; settings: " . $row['settings'] . "</br>";
+<?php 
+ require 'php/db.php';
+ foreach (statuses() as $key => $value) {
+  echo $key . ": status: " . $value['status'] . "; settings: " . $value['settings'];
+  echo "<br>";
 }
-// Close the database connection
-mysql_close();
 ?>
 </body>
 </html>
