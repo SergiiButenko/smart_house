a3990508aa42b8e03e223a72b03d3fbf31979d78
Sergii Butenko <sergii.butenko@globallogic.com>
2016-10-21 15:56:17 +0300

    Changed html to php

--- main.php
+++ main.php
@@ -1,9 +1,10 @@
 <html>
 <head>
-<title>Retrieve data from database </title>
+<title>Retrieve data from database 2</title>
 </head>
 <body>
-<?php require 'php/db.php';
+<?php 
+require 'php/db.php';
 $varr = statuses();
 print_r($varr);
 ?>
