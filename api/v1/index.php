1f8e065ed2ee84285ae4ab75250ab7f0135b9a1e
Sergii Butenko <butenko2003@ukr.net>
2016-11-03 00:32:10 +0200

    added api/v1

---
+++ api/v1/index.php
@@ -0,0 +1,16 @@
+<?php
+try{
+    // Use in the "Post-Receive URLs" section of your GitHub repo.
+	switch($_SERVER['REQUEST_METHOD'])
+	{
+	case 'GET': $the_request = &$_GET; break;
+	case 'POST': $the_request = &$_POST; break;
+	default:
+       echo "Works fine.";
+	}
+	echo $the_request;
+
+} catch ( Exception $e ) {
+    echo $e->getMessage();
+  }
+?>
