d6df3e6f303738b1077e19126e19e6fc04fc3d46
Sergii Butenko <butenko2003@ukr.net>
2016-11-03 01:52:31 +0200

    change src

--- v2/js/private.js
+++ v2/js/private.js
@@ -1,6 +1,24 @@
+$( document ).ready(function() {
+	alert(get_image_path(document.getElementById("hall")));
+  	document.getElementById("hall").src = get_image_path(document.getElementById("hall"));
+	document.getElementById("kids").src = get_image_path(document.getElementById("kids"));
+	document.getElementById("bedroom").src = get_image_path(document.getElementById("bedroom"));
+    });
+
 function changeImagePath(img) {
 	if (img.src.match(/airmoving.gif/))
     	img.src = "../images/airmoving_static.gif";
 	else if (img.src.match(/airmoving_static.gif/))
     	img.src = "../images/airmoving.gif";
    }
+
+ function get_image_path(img){
+ 	var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open( "GET", "http://butenko.asuscomm.com/api/v1?name="+img.id, true ); // false for synchronous request
+    var reponce = xmlHttp.send(null);
+    reponce = JSON.parse(reponce);
+    if (reponce.status == 1)
+    	img.src = "../images/airmoving_static.gif";
+	else 
+    	img.src = "../images/airmoving.gif";
+ }
