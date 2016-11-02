cb68cf9196f1fb8e12d2b8ff623aca86971d34ea
Sergii Butenko <butenko2003@ukr.net>
2016-11-03 00:47:10 +0200

    Added private files

--- api/v1/index.php
+++ api/v1/index.php
@@ -1,15 +1,20 @@
 <?php
+require '../../php/db.php';
 try{
     // Use in the "Post-Receive URLs" section of your GitHub repo.
 	switch($_SERVER['REQUEST_METHOD'])
 	{
-	case 'GET': $the_request = &$_GET; break;
+	case 'GET': $the_request = &$_GET; 
+	foreach (statuses($_GET['name']) as $key => $value) {
+  		echo $key . ": status: " . $value['status'] . "; settings: " . $value['settings'];
+  		echo "<br>";
+ 		}
+	break;
 	case 'POST': $the_request = &$_POST; break;
 	default:
        echo "Works fine.";
 	}
 	print_r($the_request);
-
 } catch ( Exception $e ) {
     echo $e->getMessage();
   }
