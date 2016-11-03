d538b46d8cbb2d8735496e51507f631fb4676a61
Sergii Butenko <butenko2003@ukr.net>
2016-11-03 02:28:06 +0200

    try

--- v2/js/private.js
+++ v2/js/private.js
@@ -1,10 +1,8 @@
 $( document ).ready(function() {
-	alert(get_image_path(document.getElementById("hall")));
   	document.getElementById("hall").src = get_image_path(document.getElementById("hall"));
 	document.getElementById("kids").src = get_image_path(document.getElementById("kids"));
 	document.getElementById("bedroom").src = get_image_path(document.getElementById("bedroom"));
     });
-
 
 
 function changeImagePath(img) {
@@ -14,16 +12,13 @@ function changeImagePath(img) {
     	img.src = "../images/airmoving.gif";
    }
 
- function get_image_path(img){
- 	var xmlHttp = new XMLHttpRequest();
- 	alert("http://butenko.asuscomm.com/api/v1?name="+img.id);
-    xmlHttp.open( "GET", "http://butenko.asuscomm.com/api/v1?name="+img.id, true ); // false for synchronous request
-    var reponce = xmlHttp.send(null);
-    alert(reponce);
-    reponce = JSON.parse(reponce);
-    alert(reponce);
-    if (reponce.status == 1)
-    	img.src = "../images/airmoving_static.gif";
-	else 
-    	img.src = "../images/airmoving.gif";
- }
+function get_image_path(img){
+	$.get("http://butenko.asuscomm.com/api/v1", {name: img.id})
+		.done(function(data) {
+		  cond = JSON.parse(data)[img.id];
+		  if (cond.status == 1)
+    			img.src = "../images/airmoving_static.gif";
+		  else 
+    			img.src = "../images/airmoving.gif";
+		});
+}
