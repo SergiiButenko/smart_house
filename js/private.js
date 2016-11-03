617fdaae84a6b0139d504d2de4a8710f76c76a76
Sergii Butenko <butenko2003@ukr.net>
2016-11-03 05:27:07 +0200

    replaced post with get

--- js/private.js
+++ js/private.js
@@ -9,32 +9,38 @@ function turn_on_off(img) {
 $.get("http://butenko.asuscomm.com/api/v1", {name: img.id})
 		.done(function(data) {
 		  cond = JSON.parse(data)[img.id];
-		  set_conditioner_status(!cond.status);
+		  set_conditioner_status(img.id, +!cond.status);
 		  set_image_path(img);
 		});
 }
 
 function set_image_path(img) {
-	$.get("http://butenko.asuscomm.com/api/v1", {name: img.id})
-		.done(function(data) {
-		  cond = JSON.parse(data)[img.id];		
-		  if (cond.status == 0)
-    			img.src = "images/airmoving_static.gif";
-		  else 
-    			img.src = "images/airmoving.gif";
-		});
-		  
+	  $.ajax({
+         url: "http://butenko.asuscomm.com/api/v1",
+         data: {name: img.id},
+         type: "GET",
+         success: function(data) { 
+         	cond = JSON.parse(data)[img.id];		
+			  if (cond.status == 0)
+	    			img.src = "images/airmoving_static.gif";
+			  else 
+	    			img.src = "images/airmoving.gif";
+	    }
+      });	  
 }
 
 function set_conditioner_status(id, status, settings){
 	$.ajax({
-	   url: "http://butenko.asuscomm.com/api/v1",
-	   type: 'PUT',
-	   headers: {"X-HTTP-Method-Override": "PUT"},
-	   data: "name="+id+"&status="+status+"&settings="+settings,
-	   success: function(data) {
-	     cond = JSON.parse(data)[id];
-	      alert(cond.status);
-	   }
-});
+         url: "http://butenko.asuscomm.com/api/v1",
+         data: { name: id, status: status, settings: settings},
+         type: "GET",
+		 headers: {'X_ACTION':'write',
+		 'Content-Type':'application/x-www-form-urlencoded',
+		'Access-Control-Allow-Origin': '*'},
+
+         success: function(data) { 
+         	cond = JSON.parse(data)[id];
+	        alert(cond.status); 
+	    }
+      });
 }
