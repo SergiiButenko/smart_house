d2d9686f4f36179a6a7ad5938c572e6dafabef41
Sergii Butenko <butenko2003@ukr.net>
2016-11-03 02:30:59 +0200

    Changed version

--- index.php
+++ index.php
@@ -1,23 +1,111 @@
-<html>
+<!DOCTYPE html>
+<html lang="en">
+
 <head>
-<title>Contidioners statuses</title>
+    <!-- jQuery -->
+    <script src="js/jquery.js"></script>
+    <!-- Bootstrap Core JavaScript -->
+    <script src="js/bootstrap.min.js"></script>
+    <!-- Custom JavaScript -->
+    <script src="js/private.js"></script>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <!-- Custom CSS -->
+    <link href="css/custom.css" rel="stylesheet">
+
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Bare - Start Bootstrap Template</title>
+
+
+    <style>
+    body {
+        padding-top: 70px;
+        /* Required padding for .navbar-fixed-top. Remove if using .navbar-static-top. Change if height of navigation changes. */
+
+    }
+    </style>
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
 </head>
+
 <body>
-<?php 
- require 'php/db.php';
- require 'php/check_mobile.php';
- 
- foreach (statuses() as $key => $value) {
-  echo $key . ": status: " . $value['status'] . "; settings: " . $value['settings'];
-  echo "<br>";
- }
-  
-  echo "<br>";
-  if (is_mobile()){
-      echo 'It is mobile'; 
-    } else {
-      echo 'No mobile browser detected.';
-    } 
-?>
+
+    <!-- Navigation -->
+    <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+        <div class="container">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="#">On/Off</a>
+            </div>
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav">
+                    <li>
+                        <a href="#">Schedule</a>
+                    </li>
+                </ul>
+            </div>
+            <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container -->
+    </nav>
+
+    <!-- Page Content -->
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12 text-center">
+                <h1>Turn On/Off conditioner by clicking on it</h1>
+
+                <table align="center" style="width:50%">
+                <tr>
+                <th width="100px"></th>
+                <th width="150px"></th>
+                <th class="text-center">Temperature</th>
+                </tr>
+
+                  <tr>
+                    <td class="text-left" style="font-weight:bold">Hall</td>
+                    <td><img id="hall" src="../images/airmoving.gif"  width="150" height="100" onclick="changeImagePath(this)" /></td>
+                    <td> 25 C* </td>
+                  </tr>  
+                  <tr>
+                    <td class="text-left" style="font-weight:bold">Bedroom</td>
+                    <td><img id="bedroom" src="../images/airmoving.gif"  width="150" height="100" onclick="changeImagePath(this)" /></td>
+                    <td> 25 C* </td>
+                  </tr>
+                  <tr>
+                    <td class="text-left" style="font-weight:bold">Kid's room</td>
+                    <td><img id="kids" src="../images/airmoving.gif"  width="150" height="100" onclick="changeImagePath(this)" /></td>
+                    <td> 25 C* </td>
+                  </tr>
+                </table>
+                </div>
+            </div>
+        </div>
+        <!-- /.row -->
+
+    </div>
+    <!-- /.container -->
+
 </body>
+
 </html>
