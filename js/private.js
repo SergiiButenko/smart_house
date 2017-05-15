e43a677fea0a3b88cbb9e5c4cc5c4e890cec4634
Sergii Butenko <butenko2003@ukr.net>
2017-05-15 23:08:16 +0300

    Modified modal onCLose function

--- js/private.js
+++ js/private.js
@@ -1,12 +1,29 @@
 $(document).ready(function(){
+
+   //Assign onClick for close buttons on Modal window
+   $(".modal_close").click(function(){ 
+   	id=$('#time_modal').data('id');
+   	$('#'+id).bootstrapToggle('off')
+    $('#time_modal').modal('toggle');
+   });
+
+
+	$('.switchers').bootstrapToggle({
+		 on: 'Остановить Полив',
+		 off: 'Начать полив'
+	});
+
    //Assign onChange for all switchers, so they open modal window
-   $(".switchers").change(function(){ // Click to only happen on announce links
-   	index = $(this).data('id')
-   	console.log("index="+index);
-   	name = get_branch_name(index);
-   	console.log("branch name="+name);
-   	// $("#modal_title").text(name);
-    $('#time_modal').modal('show');
+   $(".switchers").change(function(){ 
+   	if ($(this).prop('checked')){
+	   	index = $(this).data('id')
+	   	name = get_branch_name(index);
+	   	
+	   	$('#time_modal').data('id',index);
+	   	$('.modal-title').html(name);
+
+	    $('#time_modal').modal('show');
+	}
    });
 
 });
@@ -14,7 +31,7 @@ $(document).ready(function(){
 
 //Vocabulary for all branches
 function get_branch_name(index){
-	var names=[
+	var names=[ '', // Arduino stars numeration from 1. So skiping 0 index
 	'Сад, левая ветка',
 	'Сад, правая ветка'
 	];
