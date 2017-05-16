7335372e0d8cc5797415cd495f179bb012f33b28
Sergii Butenko <sergii.butenko@globallogic.com>
2017-05-16 17:37:47 +0300

    Try to update repo via git webhook

--- FlaskApp/FlaskApp/__init__.py
+++ FlaskApp/FlaskApp/__init__.py
@@ -4,20 +4,21 @@ import subprocess
 app = Flask(__name__)
 @app.route("/")
 def hello():
-    return "Hello, I love Digital Ocean!"
+	return "Hello, I love Digital Ocean!"
 
 @app.route('/gitwebhook', methods=['POST'])
 def git_post():
-	command = "cd /var/www; git reset --hard HEAD; git pull"
+	command = "cd /var/www; git reset --hard HEAD; git pull; /etc/init.d/apache2 reload"
+	res = ''
 	try: 
 		res = subprocess.check_output( [command], shell=True) 
 	except subprocess.CalledProcessError as e:
 		return "An error occurred while trying to update git repo"
-    return "Result: {0}".format(res)
+	return "Result: {0}".format(res)
 
 @app.route('/gitwebhook', methods=['GET'])
 def git_get():
-    return "Webhooks work! Now2"
+	return "Webhooks work! Now2"
 
 if __name__ == "__main__":
-    app.run()
+	app.run()
