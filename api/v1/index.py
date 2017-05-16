2c35ea33394eb8a358d64103e35e5abd6b20a508
Sergii Butenko <sergii.butenko@globallogic.com>
2017-05-16 12:53:52 +0300

    Test pyton WSGI-4

--- api/v1/index.py
+++ api/v1/index.py
@@ -1,9 +1,7 @@
-#!bin/bash
-import sys
-
-from mod_python import apache
-
-def handler(req):
-  req.content_type = 'text/plain'
-  req.write("hello world! " + sys.version)
-  return apache.OK
+ #!/usr/bin/python
+ # -*- coding: UTF-8 -*-# enable debugging
+ import cgitb
+ cgitb.enable()    
+ print("Content-Type: text/html;charset=utf-8")
+ print()    
+ print("Hello World!")
