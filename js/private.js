96998bf2fd73f824f8c15fa81930a51ef5e377fd
Sergii Butenko <sergii.butenko@globallogic.com>
2017-05-19 15:54:22 +0300

    Integrated with arduino

--- js/private.js
+++ js/private.js
@@ -6,7 +6,9 @@ var branch_names=[ '', // Arduino stars numeration from 1. So skiping 0 index
 	'Вторая ветка',
 	'Третья ветка',
 	'Четвертая ветка',
-	'Пятая ветка'
+	'Пятая ветка',
+	'',
+	'Насос'
 	];
 
 $(document).ready(function(){
@@ -17,7 +19,7 @@ $(document).ready(function(){
    //Assign onClick for close buttons on Modal window
    $(".modal_close").click(function(){ 
    	id=$('#time_modal').data('id');
-   	$('#'+id).bootstrapToggle('off')
+   	$('#'+id).bootstrapToggle('off');
     $('#time_modal').modal('hide');
    });
 
@@ -29,29 +31,153 @@ $(document).ready(function(){
 
    //Assign onChange for all switchers, so they open modal window
    $(".switchers").change(function(){ 
-   	if ($(this).prop('checked')){
-	   	index = $(this).data('id')
+   	if ($(this).prop('checked') && $(this).data('user-action') == 1){
+	   	index = $(this).data('id');
 	   	name = branch_names[index];
 	   	
 	   	$('#time_modal').data('id',index);
 	   	$('.modal-title').html(name);
 
 	    $('#time_modal').modal('show');
+	} 
+
+	if (!$(this).prop('checked') && $(this).data('user-action') == 1){
+
+	   	index = $(this).data('id');
+	   	branch_off(index);
 	}
    });
 
-	(function worker() {
-	  $.ajax({
-	    url: aruino_ip, 
+   //Function to start irrigation
+   $(".start-irrigation").click(function(){ 
+   	index = $('#time_modal').data('id');
+   	branch_on(index);
+   });
+
+	// (function worker() {
+	//   $.ajax({
+	//     url: aruino_ip, 
+	//     success: function(data) {
+	//       update_branches(JSON.stringify(data));
+	//       $("#for_test").html("DEMON updates each 3sec: "+JSON.stringify(data));
+	//     },
+	//     complete: function() {
+	//       // Schedule the next request when the current one's complete
+	//       setTimeout(worker, arduino_timeout_sec*1000);
+	//     }
+	//   });
+	// })();
+});
+
+
+function branch_on(index){
+	$.ajax({
+	    url: aruino_ip+'/on', 
+	    type: "get",
+	    data: {
+	    	'params' : index
+	    },
 	    success: function(data) {
-	      $("#for_test").html("DEMON updates each 3sec: "+JSON.stringify(data));
+	      console.log('Line '+index+' activated');
 	    },
 	    complete: function() {
-	      // Schedule the next request when the current one's complete
-	      setTimeout(worker, arduino_timeout_sec*1000);
+	      update_branches_request();
+	      console.log('done');
 	    }
 	  });
-	})();
+}
+
+function branch_off(index){
+	$.ajax({
+	    url: aruino_ip+'/off', 
+	    type: "get",
+	    data: {
+	    	'params' : index
+	    },
+	    success: function(data) {
+	      console.log('Line '+index+' deactivated');
+	    },
+	    complete: function() {
+	      update_branches_request();
+	      console.log('done');
+	    }
+	  });
+}
+
+function update_branches_request(json){
+	$.ajax({
+	    url: aruino_ip, 
+	    success: function(data) {
+			branches = data['variables'];
+
+			$('#1').data('user-action', 0);
+			$('#1').bootstrapToggle(get_state(branches['1']));	
+			$('#1').data('user-action', 1);
+
+			$('#2').data('user-action', 0);
+			$('#2').bootstrapToggle(get_state(branches['2']));	
+			$('#2').data('user-action', 1);
+
+			$('#3').data('user-action', 0);
+			$('#3').bootstrapToggle(get_state(branches['3']));	
+			$('#3').data('user-action', 1);
+
+			$('#4').data('user-action', 0);
+			$('#4').bootstrapToggle(get_state(branches['4']));	
+			$('#4').data('user-action', 1);
+
+			$('#5').data('user-action', 0);
+			$('#5').bootstrapToggle(get_state(branches['5']));	
+			$('#5').data('user-action', 1);
+
+			$('#7').data('user-action', 0);
+			$('#7').bootstrapToggle(get_state(branches['pump']));	
+			$('#7').data('user-action', 1);
+
+			function get_state(i){
+				if (i==0)
+				 return 'off';
+				else
+				  return 'on';
+			}
+	    }
+	  });
+}
+
+function update_branches(json){
+	json = JSON.parse(json);
+	branches = json['variables'];
+	console.log();
+
+	$('#1').data('user-action', 0);
+	$('#1').bootstrapToggle(get_state(branches['1']));	
+	$('#1').data('user-action', 1);
+
+	$('#2').data('user-action', 0);
+	$('#2').bootstrapToggle(get_state(branches['2']));	
+	$('#2').data('user-action', 1);
+
+	$('#3').data('user-action', 0);
+	$('#3').bootstrapToggle(get_state(branches['3']));	
+	$('#3').data('user-action', 1);
+
+	$('#4').data('user-action', 0);
+	$('#4').bootstrapToggle(get_state(branches['4']));	
+	$('#4').data('user-action', 1);
+
+	$('#5').data('user-action', 0);
+	$('#5').bootstrapToggle(get_state(branches['5']));	
+	$('#5').data('user-action', 1);
+
+	$('#7').data('user-action', 0);
+	$('#7').bootstrapToggle(get_state(branches['pump']));	
+	$('#7').data('user-action', 1);
 
 
-});
+	function get_state(i){
+		if (i==0)
+		 return 'off';
+		else
+		  return 'on';
+	}
+}
