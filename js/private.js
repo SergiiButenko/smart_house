953689a2ed2b12c4309b693ae83cc800bd1fa01f
Sergii <butenko2003@ukr.net>
2017-05-21 15:18:16 +0300

    Changed arduino IP

--- js/private.js
+++ js/private.js
@@ -1,4 +1,4 @@
-var aruino_ip='http://butenko.asuscomm.com:5555';
+var aruino_ip='http://192.168.1.10';
 var arduino_timeout_sec=3;
 
 var branch_names=[ '', // Arduino stars numeration from 1. So skiping 0 index
