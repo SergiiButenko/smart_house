d4da1c024fdaacd38275ef7757580eea8f5ae5af
Пользователь <>
2017-05-22 00:43:48 +0300

    Changed bash

--- tools/scripts/repo_update.sh
+++ tools/scripts/repo_update.sh
@@ -3,6 +3,6 @@ git reset --hard
 git pull
 version=`git rev-parse --short HEAD`
 msg=`git log -1 --pretty=%B`
-sed -n "s/test/$version;$msg/g" /var/www/index.html
+sed -ni "s/test/$version;$msg/g" /var/www/index.html
 echo 'HEAD is now '$msg
 /etc/init.d/apache2 reload
