517d5b3a86544a40bd56988e78f35d43b78cc338
Sergii Butenko <sergii.butenko@globallogic.com>
2017-05-22 16:24:34 +0300

    added script for restart apache

--- tools/scripts/repo_update.sh
+++ tools/scripts/repo_update.sh
@@ -5,4 +5,3 @@ version=`git rev-parse --short HEAD`
 msg=`git log -1 --pretty=%B | tr -s ' ' | tr ' ' '_'`
 sed -i "s|test|$msg|g" /var/www/index.html
 echo 'HEAD is now '$msg
-/etc/init.d/apache2 reload
