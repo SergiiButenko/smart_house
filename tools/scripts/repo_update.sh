7969cdc16688f71b909ae2b2b6a969c87a9bec34
Sergii Butenko <sergii.butenko@globallogic.com>
2017-05-22 16:20:41 +0300

    Changed deploty script

--- tools/scripts/repo_update.sh
+++ tools/scripts/repo_update.sh
@@ -2,7 +2,7 @@ cd /var/www
 git reset --hard
 git pull
 version=`git rev-parse --short HEAD`
-msg=`git log -1 --pretty=%B`
-sed -i "s/test/$version;$msg/g" /var/www/index.html
+msg=`git log -1 --pretty=%B | tr -s ' ' | tr ' ' '_'`
+sed -i "s|test|$msg|g" /var/www/index.html
 echo 'HEAD is now '$msg
 /etc/init.d/apache2 reload
