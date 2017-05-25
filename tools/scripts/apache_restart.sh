1b27bcf7b67c7bd47a4106d23c8a4c4f408463d2
Sergii Butenko <sergii.butenko@globallogic.com>
2017-05-25 19:21:51 +0300

    changed message

--- tools/scripts/apache_restart.sh
+++ tools/scripts/apache_restart.sh
@@ -3,6 +3,6 @@ git reset --hard
 git pull
 version=`git rev-parse --short HEAD`
 msg=`git log -1 --pretty=%B | tr -s ' ' | tr ' ' '_'`
-sed -i "s|test|$msg|g" /var/www/beta.html
+sed -i "s|test|commit: $msg|g" /var/www/beta.html
 echo 'HEAD is now '$msg
 /etc/init.d/apache2 reload
