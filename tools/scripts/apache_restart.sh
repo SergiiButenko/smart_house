7af5beb01bc1afc7e0e00ed25ec63306f7aeecc2
Sergii Butenko <sergii.butenko@globallogic.com>
2017-05-26 18:10:09 +0300

    Changed deploy script

--- tools/scripts/apache_restart.sh
+++
@@ -1,9 +0,0 @@
-cd /var/www
-git reset --hard
-git pull
-version=`git rev-parse --short HEAD`
-msg=`git log -1 --pretty=%B | tr -s ' ' | tr ' ' '_'`
-sed -i "s|test|commit: $msg|g" /var/www/beta.html
-sed -i "s|test|commit: $msg|g" /var/www/index.html
-echo 'HEAD is now '$msg
-/etc/init.d/apache2 reload
--- tools/scripts/repo_update.sh
+++ tools/scripts/repo_update.sh
@@ -3,4 +3,7 @@ git reset --hard
 git pull
 msg=`git log -1 --pretty=%B | tr -s ' ' | tr ' ' '_'`
 sed -i "s|test|$msg|g" /var/www/index.html
+sed -i "s|test|$msg|g" /var/www/beta.html
 echo 'HEAD is now '$msg
+
+systemctl restart irrigation_backend.service
