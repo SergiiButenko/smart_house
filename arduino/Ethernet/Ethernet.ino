73521e790975996c80ae71b8ad479106b74c4263
Sergii Butenko <sergii.butenko@globallogic.com>
2017-05-26 18:29:42 +0300

    Changed arduino script

--- arduino/Ethernet/Ethernet.ino
+++ arduino/Ethernet/Ethernet.ino
@@ -25,14 +25,23 @@ EthernetServer server(80);
 aREST rest = aREST();
 
 // Variables to be exposed to the API
-int arduino_status = 1;
+int branch_1_status;
+int branch_2_status;
+int branch_3_status;
+int branch_4_status;
+int branch_5_status;
+int branch_6_status;
+int branch_7_status;
+int pump_status;
 
 int branch_1 = 2;
 int branch_2 = 3;
-int branch_3 = 5;
-int branch_4 = 6;
-int branch_5 = 8;
-int pump = 7;
+int branch_3 = 4;
+int branch_4 = 5;
+int branch_5 = 6;
+int branch_6 = 7;
+int branch_7 = 8;
+int pump = 9;
 
 void setup(void)
 {
@@ -45,33 +54,30 @@ void setup(void)
   pinMode(branch_3, OUTPUT);
   pinMode(branch_4, OUTPUT);
   pinMode(branch_5, OUTPUT);
+  pinMode(branch_6, OUTPUT);
+  pinMode(branch_7, OUTPUT);
   pinMode(pump, OUTPUT);
 
 
   // Init variables and expose them to REST API
-  rest.variable("status",&arduino_status);
+ 
+rest.variable("1",&branch_1_status);
+rest.variable("2",&branch_2_status);
+rest.variable("3",&branch_3_status);
+rest.variable("4",&branch_4_status);
+rest.variable("5",&branch_5_status);
+rest.variable("6",&branch_5_status);
+rest.variable("7",&branch_5_status);
+rest.variable("pump",&pump_status);  
+
 
   // Function to be exposed
-//  rest.function("branch1On",branch1On);
-//  rest.function("branch2On",branch2On);
-//  rest.function("branch3On",branch3On);
-//  rest.function("branch4On",branch4On);
-//  rest.function("branch5On",branch5On);
-  
-  rest.function("branch1Off",branch1Off);
-  rest.function("branch2Off",branch2Off);
-  rest.function("branch3Off",branch3Off);
-  rest.function("branch4Off",branch4Off);
-  rest.function("branch5Off",branch5Off);
-//
-//  rest.function("pumpOn",pumpOn);
-//  rest.function("pumpOff",pumpOff);
-
-  //rest.function("branches_status",branches_status);
+  rest.function("on",on);
+  rest.function("off",off);
   
   // Give name & ID to the device (ID should be 6 characters long)
   rest.set_id("008");
-  rest.set_name("irrigation_peregonivka");
+  rest.set_name("name");
 
   // Start the Ethernet connection and the server
   if (Ethernet.begin(mac) == 0) {
@@ -92,107 +98,77 @@ void loop() {
   // listen for incoming clients
   EthernetClient client = server.available();
   rest.handle(client);
-  wdt_reset();
+  wdt_reset(); 
 }
 
-
-int test(){
-  return 0;
+void branches_status(){
+  branch_1_status = digitalRead(branch_1);
+  branch_2_status = digitalRead(branch_2);
+  branch_3_status = digitalRead(branch_3);
+  branch_4_status = digitalRead(branch_4);
+  branch_5_status = digitalRead(branch_5);
+  branch_6_status = digitalRead(branch_6);
+  branch_7_status = digitalRead(branch_7);
+  pump_status = digitalRead(pump);
 }
 
-// Custom function accessible by the API
-int branch1On(){
-  digitalWrite(branch_1, HIGH);
+bool if_no_branch_active(){
+  if (branch_1_status==LOW and branch_2_status==LOW and branch_3_status==LOW and branch_4_status==LOW and branch_5_status==LOW
+    and branch_5_status==LOW and branch_7_status==LOW){
+    return true;
+  }
+  return false;
+}
+
+int get_branch_pin(int i){
+  if (i==1){
+    return branch_1;
+  }
+  if (i==2){
+    return branch_2;
+  }
+  if (i==3){
+    return branch_3;
+  }
+  if (i==4){
+    return branch_4;
+  }
+  if (i==5){
+    return branch_5;
+  }
+  if (i==6){
+    return branch_6;
+  }
+  if (i==7){
+    return branch_7;
+  }
+  if (i==8){
+    return pump;
+  }
+}
+
+int on(String branch){
+  // Get state from command
+  int pin = get_branch_pin(branch.toInt());
+  Serial.println(pin);
+  digitalWrite(pin,HIGH);
   digitalWrite(pump, HIGH);
 
-  return digitalRead(branch_1);
+  branches_status();
+  return digitalRead(pin);
 }
 
-int branch1Off(){
-  digitalWrite(branch_1, LOW);
-  digitalWrite(pump, LOW);
+int off(String branch){
+  // Get state from command
+  int pin = get_branch_pin(branch.toInt());
 
-  return digitalRead(branch_1);
+  digitalWrite(pin,LOW);
+  branches_status();
+  
+  if ( if_no_branch_active() ){
+    digitalWrite(pump, LOW);
+  }
+
+  branches_status();
+  return digitalRead(pin);
 }
-
-int branch2On(){
-  digitalWrite(branch_2, HIGH);
-  digitalWrite(pump, HIGH);
-
-  return digitalRead(branch_2);
-}
-
-int branch2Off(){
-  digitalWrite(branch_2, LOW);
-  digitalWrite(pump, LOW);
-
-  return digitalRead(branch_2);
-}
-
-int branch3On(){
-  digitalWrite(branch_3, HIGH);
-  digitalWrite(pump, HIGH);
-
-  return digitalRead(branch_3);
-}
-
-int branch3Off(){
-  digitalWrite(branch_3, LOW);
-  digitalWrite(pump, LOW);
-
-  return digitalRead(branch_3);
-}
-
-int branch4On(){
-  digitalWrite(branch_4, HIGH);
-  digitalWrite(pump, HIGH);
-
-  return digitalRead(branch_4);
-}
-
-int branch4Off(){
-  digitalWrite(branch_4, LOW);
-  digitalWrite(pump, LOW);
-
-  return digitalRead(branch_4);
-}
-
-int branch5On(){
-  digitalWrite(branch_5, HIGH);
-  digitalWrite(pump, HIGH);
-
-  return digitalRead(branch_5);
-}
-
-int branch5Off(){
-  digitalWrite(branch_5, LOW);
-  digitalWrite(pump, LOW);
-
-  return digitalRead(branch_5);
-}
-
-int pumpOn(){
-  digitalWrite(pump, HIGH);
-
-  return digitalRead(pump);
-}
-
-
-int pumpOff(){
-  digitalWrite(pump, LOW);
-
-  return digitalRead(pump);
-}
-
-
-//char branches_status(){
-//  char res[6];
-//  res[0]=digitalRead(branch_1);
-//  res[1]=digitalRead(branch_2);
-//  res[2]=digitalRead(branch_3);
-//  res[3]=digitalRead(branch_4);
-//  res[4]=digitalRead(branch_5);
-//  res[5]=digitalRead(pump);
-//
-//  return res;
-//}
