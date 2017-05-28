a02bc3bcc19133a5bcbf6c374af70a3318aece3c
Oleg <tomicdog@gmail.com>
2017-05-28 23:55:04 +0300

    Changed type time to type timestamp in table life

--- sql/insert_rules_to_life.sql
+++ sql/insert_rules_to_life.sql
@@ -1,3 +1,3 @@
-insert into life (line_id, rule_id, timer, date) select w.line_id, w.rule_id,w.time, current_date from week_schedule as w where w.day_number = date_part('dow', now());
+insert into life (line_id, rule_id, timer, date) select w.line_id, w.rule_id,current_date +  w.time, current_date from week_schedule as w where w.day_number = date_part('dow', now());
 
-insert into life (line_id, rule_id, timer, date) select w.line_id, 2,w.time + w.interval*interval '1 minute', current_date from week_schedule as w where w.day_number = date_part('dow', now()) and w.rule_id = 1;
+insert into life (line_id, rule_id, timer, date) select w.line_id, 2,current_date + w.time + w.interval*interval '1 minute', current_date from week_schedule as w where w.day_number = date_part('dow', now()) and w.rule_id = 1;
