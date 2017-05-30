0f0d4c527241ea7726c9ec18cfd51a72c37d3356
Sergii Butenko <sergii.butenko@globallogic.com>
2017-05-30 15:08:53 +0300

    Changed dropdown mobile3

--- js/beta.js
+++ js/beta.js
@@ -28,7 +28,6 @@ $(document).ready(function() {
 
 
     if( /Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent) ) {
-      $('.selectpicker').selectpicker('mobile');
       $('.selectpicker').addClass("mobile-device");
     }
 
