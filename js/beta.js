ba858216e7c5f5779780ed0e0e61f7ffe3f071ad
Sergii Butenko <sergii.butenko@globallogic.com>
2017-05-30 13:43:44 +0300

    Selectpicker for mobile

--- js/beta.js
+++ js/beta.js
@@ -20,14 +20,14 @@ $(document).ready(function() {
         $('#title-' + i + " span").text(branch_names[i]);
     }
 
-    // if( /Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent) ) {
-    //   $('.selectpicker').selectpicker('mobile');
-    // }
+    if( /Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent) ) {
+      $('#time_selector').selectpicker('mobile');
+    }
 
     for (var i=1; i<=20; i++){
      $('#time_selector').append("<option data-value="+i+" id=\"option"+i+"\">"+i+"</option>");
     }
-    $('.selectpicker').selectpicker('refresh');
+    $('#time_selector').selectpicker('refresh');
 
     var $loading = $('#loader').hide();
     $(document)
