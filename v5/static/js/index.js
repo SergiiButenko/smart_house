0d18bf87e2d371c39c8019fa0d2ad01663b3d214
Sergii Butenko <sergii.butenko@globallogic.com>
2017-08-30 16:01:26 +0300

    removed custom js and css

--- v5/static/js/index.js
+++ v5/static/js/index.js
@@ -207,6 +207,37 @@ $(document).ready(function() {
         branch_off(index);
     });
 
+
+    //comming from template
+    var buttons = ["drawer-f-l", "drawer-f-r", "drawer-f-t", "drawer-f-b"]
+
+  $.each(buttons, function (index, position) {
+    $('#' + position).click(function() {
+      setDrawerPosition('bmd-' + position)
+    })
+  })
+
+  // add a toggle for drawer visibility that shows anytime
+  $('#drawer-visibility').click(function () {
+    var $container = $('.bmd-layout-container')
+
+    // once clicked, just do away with responsive marker
+    //$container.removeClass('bmd-drawer-in-md')
+
+    var $btn = $(this)
+    var $icon = $btn.find('.material-icons')
+    if ($icon.text() == 'visibility') {
+      $container.addClass('bmd-drawer-out') // demo only, regardless of the responsive class, we want to force it close
+      $icon.text('visibility_off')
+      $btn.attr('title', 'Drawer allow responsive opening')
+    }
+    else {
+      $container.removeClass('bmd-drawer-out') // demo only, regardless of the responsive class, we want to force it open
+      $icon.text('visibility')
+      $btn.attr('title', 'Drawer force closed')
+    }
+  })
+
 });
 
 function branch_on(index, time_minutes, interval_quantity, time_wait) {
@@ -392,4 +423,22 @@ function set_status_ok() {
 function set_status_spinner() {
     $("#system_status").text(class_spin.msg);
     $(".btn-open-modal").addClass('disabled');
+}
+
+
+
+// Comming from template
+function clearDrawerClasses($container) {
+  var classes = ["bmd-drawer-f-l", "bmd-drawer-f-r", "bmd-drawer-f-t", "bmd-drawer-f-b"];
+
+  $.each(classes, function (index, value) {
+    $container.removeClass(value)
+  })
+}
+
+function setDrawerPosition(position) {
+  var $container = $('.bmd-layout-container')
+
+  clearDrawerClasses($container)
+  $container.addClass(position)
 }
