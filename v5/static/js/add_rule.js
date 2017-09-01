d18c8ad2e7e2dc4c3681d3f84f2f5b836acb14d6
Sergii Butenko <sergii.butenko@globallogic.com>
2017-09-01 14:26:48 +0300

    dropdown

--- v5/static/js/add_rule.js
+++ v5/static/js/add_rule.js
@@ -15,10 +15,28 @@ $(document).ready(function() {
                     'default_interval': parseInt(item['default_interval']) || 2,
                     'default_time_wait': parseInt(item['default_time_wait']) || 15
                 }
-                 $(".selectpicker").append($('<option></option>').val('id', item['id']).html(item['name']));
+                $(".dropdown-menu").append(
+                    $('<button></button>').data('id', item['id']).html(item['name']).addClass("dropdown-item")
+                    );
             }
         }
     });
+
+    $(".dropdown-item").click(function() {
+        id = $(this).data('id')
+        
+        default_time = branch[id]['default_time'] 
+        default_interval = branch[id]['default_interval'] 
+        default_time_wait = branch[id]['default_time_wait']
+
+        $(this).parents(".dropdown").find('.btn').html($(this).text() + ' <span class="caret"></span>');
+
+        card=$(".dropdown-item").closest(".card")
+        card.data('id', id);
+        card.find('#irrigation_minutes').val(default_time);
+        card.find('#irrigation_intervals').val(default_interval);
+        card.find('#irrigation_time_wait').val(default_time_wait);
+});
 
 
     $("#add_rule_block").click(function() {
