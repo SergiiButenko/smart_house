e134c8a970b8073f29ec43dc1b81ce9586faf779
Sergii Butenko <sergii.butenko@globallogic.com>
2017-09-14 12:54:14 +0300

    debug

--- v5/static/js/index.js
+++ v5/static/js/index.js
@@ -80,6 +80,16 @@ $(document).ready(function() {
 
     socket.on('branch_status', function(msg) {
         console.log('Message received. New brach status: ');
+
+        console.log('0');
+        console.log(JSON.parse(msg.data[0]));
+        console.log('1');
+        console.log(JSON.parse(msg.data[1]));
+        console.log('json data');
+        console.log(JSON.parse(msg.data));
+        console.log('data');
+        console.log(msg.data);
+
         data = JSON.parse(msg.data[0])
         update_branches(data);
     });
