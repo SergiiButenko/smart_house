97012ab6f2bd34a5b9916280f956838db2aa15cd
Sergii Butenko <sergii.butenko@globallogic.com>
2017-09-18 14:03:00 +0300

    sll adhoc

--- v5/viber_bot.py
+++ v5/viber_bot.py
@@ -52,5 +52,5 @@ def incoming():
     return Response(status=200)
 
 if __name__ == "__main__":
-    context = ('server.crt', 'server.key')
-    app.run(host='0.0.0.0', port=443, debug=True, ssl_context=context)
+    # context = ('server.crt', 'server.key')
+    app.run(host='0.0.0.0', port=443, debug=True, ssl_context='adhoc')
