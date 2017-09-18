1a7f6ab76aec4a4095721bc97fcf1e194d44313b
Sergii Butenko <sergii.butenko@globallogic.com>
2017-09-18 14:00:04 +0300

    added bot

--- v5/viber_bot.py
+++ v5/viber_bot.py
@@ -28,15 +28,28 @@ logger.addHandler(handler)
 
 @app.route('/', methods=['POST'])
 def incoming():
-	viber_request = viber.parse_request(request.get_data())
+	logger.debug("received request. post data: {0}".format(request.get_data()))
+    # every viber message is signed, you can verify the signature using this method
+    if not viber.verify_signature(request.get_data(), request.headers.get('X-Viber-Content-Signature')):
+        return Response(status=403)
 
-	if isinstance(viber_request, ViberConversationStartedRequest):
-		viber.send_messages(viber_request.get_user().get_id(), [
-			TextMessage(text="Welcome!")
-		])
+    # this library supplies a simple way to receive a request object
+    viber_request = viber.parse_request(request.get_data())
 
-	return Response(status=200)
+    if isinstance(viber_request, ViberMessageRequest):
+        message = viber_request.message
+        # lets echo back
+        viber.send_messages(viber_request.sender.id, [
+            message
+        ])
+    elif isinstance(viber_request, ViberSubscribedRequest):
+        viber.send_messages(viber_request.get_user.id, [
+            TextMessage(text="thanks for subscribing!")
+        ])
+    elif isinstance(viber_request, ViberFailedRequest):
+        logger.warn("client failed receiving message. failure: {0}".format(viber_request))
 
+    return Response(status=200)
 
 if __name__ == "__main__":
     context = ('server.crt', 'server.key')
