b034edb2ca81900e5d234da0b54ed8818a951eb1
Sergii Butenko <sergii.butenko@globallogic.com>
2017-09-19 18:52:41 +0300

    test rich test

--- v5/viber_bot/viber_bot.py
+++ v5/viber_bot/viber_bot.py
@@ -39,17 +39,80 @@ def get_response(incom_message):
         return TextMessage(text='начать')
     elif (incom_message.text.lower() == 'тест'):
         SAMPLE_RICH_MEDIA = """{
-          "BgColor": "#69C48A",
-          "Buttons": [
-            {
-              "Columns": 6,
-              "Rows": 1,
-              "ActionType": "open-url",
-              "ActionBody": "www.tut.by",
-              "Image": "www.tut.by/img.jpg"
-            }
-          ]
-        }"""
+        "Type": "rich_media",
+        "ButtonsGroupColumns": 6,
+        "ButtonsGroupRows": 7,
+        "BgColor": "#FFFFFF",
+        "Buttons": [{
+          "Columns": 6,
+          "Rows": 3,
+          "ActionType": "open-url",
+          "ActionBody": "https://www.google.com",
+          "Image": "http://html-test:8080/myweb/guy/assets/imageRMsmall2.png"
+        }, {
+          "Columns": 6,
+          "Rows": 2,
+          "Text": "<font color=#323232><b>Headphones with Microphone, On-ear Wired earphones</b></font><font color=#777777><br>Sound Intone </font><font color=#6fc133>$17.99</font>",
+          "ActionType": "open-url",
+          "ActionBody": "https://www.google.com",
+          "TextSize": "medium",
+            "TextVAlign": "middle",
+            "TextHAlign": "left"
+        }, {
+          "Columns": 6,
+          "Rows": 1,
+          "ActionType": "reply",
+          "ActionBody": "https://www.google.com",
+          "Text": "<font color=#ffffff>Buy</font>",
+          "TextSize": "large",
+            "TextVAlign": "middle",
+            "TextHAlign": "middle",
+          "Image": "https://s14.postimg.org/4mmt4rw1t/Button.png"
+        }, {
+          "Columns": 6,
+          "Rows": 1,
+          "ActionType": "reply",
+          "ActionBody": "https://www.google.com",
+          "Text": "<font color=#8367db>MORE DETAILS</font>",
+          "TextSize": "small",
+            "TextVAlign": "middle",
+            "TextHAlign": "middle"
+        }, {
+          "Columns": 6,
+          "Rows": 3,
+          "ActionType": "open-url",
+          "ActionBody": "https://www.google.com",
+          "Image": "https://s16.postimg.org/wi8jx20wl/image_RMsmall2.png"
+        }, {
+          "Columns": 6,
+          "Rows": 2,
+          "Text": "<font color=#323232><b>Hanes Men's Humor Graphic T-Shirt</b></font><font color=#777777><br>Hanes</font><font color=#6fc133>$10.99</font>",
+          "ActionType": "open-url",
+          "ActionBody": "https://www.google.com",
+          "TextSize": "medium",
+            "TextVAlign": "middle",
+            "TextHAlign": "left"
+        }, {
+          "Columns": 6,
+          "Rows": 1,
+          "ActionType": "reply",
+          "ActionBody": "https://www.google.com",
+          "Text": "<font color=#ffffff>Buy</font>",
+          "TextSize": "large",
+            "TextVAlign": "middle",
+            "TextHAlign": "middle",
+          "Image": "https://s14.postimg.org/4mmt4rw1t/Button.png"
+        }, {
+          "Columns": 6,
+          "Rows": 1,
+          "ActionType": "reply",
+          "ActionBody": "https://www.google.com",
+          "Text": "<font color=#8367db>MORE DETAILS</font>",
+          "TextSize": "small",
+            "TextVAlign": "middle",
+            "TextHAlign": "middle"
+        }]
+      }"""
 
         SAMPLE_ALT_TEXT = "upgrade now!"
 
