04d264c0a7c3a607b652202a082ef953eaa60420
Sergii Butenko <sergii.butenko@globallogic.com>
2017-09-19 18:35:52 +0300

    test rich test

--- v5/viber_bot/viber_bot.py
+++ v5/viber_bot/viber_bot.py
@@ -2,18 +2,19 @@ from flask import Flask, request, Response
 from viberbot import Api
 from viberbot.api.bot_configuration import BotConfiguration
 from viberbot.api.messages.text_message import TextMessage
-from viberbot.api.viber_requests import ViberConversationStartedRequest
+from viberbot.api.messages.rich_media_message import RichMediaMessage
+# from viberbot.api.viber_requests import ViberConversationStartedRequest
 from viberbot.api.viber_requests import ViberFailedRequest
 from viberbot.api.viber_requests import ViberMessageRequest
 from viberbot.api.viber_requests import ViberSubscribedRequest
-from viberbot.api.viber_requests import ViberUnsubscribedRequest
-from viberbot.api.viber_requests import ViberDeliveredRequest
+# from viberbot.api.viber_requests import ViberUnsubscribedRequest
 
 
 import time
 import logging
 import sched
 import threading
+import json
 
 logger = logging.getLogger()
 logger.setLevel(logging.DEBUG)
@@ -35,27 +36,53 @@ USERS = [{'Sergii': 'cHxBN+Zz1Ldd/60xd62U/w=='}, {'Oleg': ''}, {'Irina': ''}]
 
 def get_response(incom_message):
     if (incom_message.text.lower() == 'полив'):
-        return 'начать'
+        return TextMessage(text='начать')
+    elif (incom_message.text.lower() == 'тест'):
+        SAMPLE_RICH_MEDIA = """{
+          "BgColor": "#69C48A",
+          "Buttons": [
+            {
+              "Columns": 6,
+              "Rows": 1,
+              "BgColor": "#454545",
+              "BgMediaType": "gif",
+              "BgMedia": "http://www.url.by/test.gif",
+              "BgLoop": true,
+              "ActionType": "open-url",
+              "Silent": true,
+              "ActionBody": "www.tut.by",
+              "Image": "www.tut.by/img.jpg",
+              "TextVAlign": "middle",
+              "TextHAlign": "left",
+              "Text": "<b>example</b> button",
+              "TextOpacity": 10,
+              "TextSize": "regular"
+            }
+          ]
+        }"""
+
+        SAMPLE_ALT_TEXT = "upgrade now!"
+
+        return RichMediaMessage(rich_media=SAMPLE_RICH_MEDIA, alt_text=SAMPLE_ALT_TEXT)
 
 
 @app.route('/', methods=['POST'])
 def incoming():
     logger.debug("received request. post data: {0}".format(request.get_data()))
+    if not viber.verify_signature(request.get_data(), request.headers.get('X-Viber-Content-Signature')):
+            return Response(status=403)
 
     viber_request = viber.parse_request(request.get_data().decode())
 
     if (isinstance(viber_request, ViberMessageRequest)):
-        message = viber_request.message
-        response = get_response(message)
+        message = get_response(viber_request.message)
         logger.warn("Sending message")
         viber.send_messages(viber_request.sender.id, [
-            TextMessage(text=response)
+            message
         ])
-    elif isinstance(viber_request, ViberConversationStartedRequest) \
-            or isinstance(viber_request, ViberSubscribedRequest) \
-            or isinstance(viber_request, ViberUnsubscribedRequest):
-        viber.send_messages(viber_request.user.id, [
-            TextMessage(text=viber_request.event_type())
+    elif isinstance(viber_request, ViberSubscribedRequest):
+        viber.send_messages(viber_request.get_user.id, [
+            TextMessage(text="thanks for subscribing!")
         ])
     elif isinstance(viber_request, ViberFailedRequest):
         logger.warn("client failed receiving message. failure: {0}".format(viber_request))
@@ -66,6 +93,17 @@ def incoming():
 @app.route('/send_message', methods=['POST'])
 def send_message():
     logger.debug("received request for send_message. post data: {0}".format(request.get_data()))
+    data = json.loads(request.get_data())
+    users = data['users']
+    message = data['message']
+
+    logger.info("Sending message")
+
+    for user in users:
+        logger.info("Sending message to {0}. id: {1}".format(user['name'], user['id']))
+        viber.send_messages(user['id'], [
+            TextMessage(text=message)
+        ])
 
 
 def set_webhook(viber):
