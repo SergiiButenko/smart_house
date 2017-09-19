ffb7c79863dfadb119937b431cda1e504a1b5b18
Sergii Butenko <sergii.butenko@globallogic.com>
2017-09-19 13:38:24 +0300

    Changed sh

--- tools/scripts/repo_update.sh
+++
@@ -1,9 +0,0 @@
-cd /var/www/
-git reset --hard
-git pull
-msg=`git log -1 --pretty=%B | tr -s ' ' | tr ' ' '_'`
-
-systemctl restart irrigation_beta.service
-systemctl restart rules_handler.service
-
-echo 'HEAD is now '$msg
--- tools/scripts/restart_7542.sh
+++ tools/scripts/restart_7542.sh
@@ -5,6 +5,6 @@ msg=`git log -1 --pretty=%B | tr -s ' ' | tr ' ' '_'`
 
 systemctl restart irrigation_7542.service
 systemctl restart rules_handler.service
-
+systemctl restart irrigation_viber_bot.service
 
 echo 'HEAD is now '$msg
