95f6e3c386db5190422bf38e5bb8ffb2940f08c2
Sergii Butenko <sergii.butenko@globallogic.com>
2017-09-24 13:39:21 +0300

    midified power_supply monitor

--- tools/scripts/restart_7542.sh
+++ tools/scripts/restart_7542.sh
@@ -6,5 +6,6 @@ msg=`git log -1 --pretty=%B | tr -s ' ' | tr ' ' '_'`
 systemctl restart irrigation_7542.service
 systemctl restart rules_handler.service
 systemctl restart irrigation_viber_bot.service
+systemctl restart irrigation_power_supply.service
 
 echo 'HEAD is now '$msg
