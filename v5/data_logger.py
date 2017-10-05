0804521d396eddf283cc7139dcd7062ca23c010d
sergii <butenko2003@ukr.net>
2017-10-06 01:51:34 +0300

    Added power outlet

--- v5/data_logger.py
+++ v5/data_logger.py
@@ -126,13 +126,13 @@ def weather():
             temperature_small_h_2_fl = 0
             humidity_small_h_2_fl = 0
 
-        logging.info(QUERY[mn()].format(temperature_street, humidity_street,
+        logging.info(QUERY[mn()].format(0, humidity_street,
             temperature_small_h_1_fl, humidity_small_h_1_fl,
             temperature_small_h_2_fl, humidity_small_h_2_fl,
             temperature_big_h_1_fl, humidity_big_h_1_fl,
             temperature_big_h_2_fl, humidity_big_h_2_fl))
 
-        update_db_request(QUERY[mn()].format(temperature_street, humidity_street,
+        update_db_request(QUERY[mn()].format(0, humidity_street,
             temperature_small_h_1_fl, humidity_small_h_1_fl,
             temperature_small_h_2_fl, humidity_small_h_2_fl,
             temperature_big_h_1_fl, humidity_big_h_1_fl,
