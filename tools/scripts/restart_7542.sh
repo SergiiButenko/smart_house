3c22cff92ad232e3f79197152f0f4de5feea020d
sergii <butenko2003@ukr.net>
2017-10-22 22:45:08 +0300

    replaced old endpoints for ui

--- tools/scripts/restart_7542.sh
+++ tools/scripts/restart_7542.sh
@@ -5,7 +5,7 @@ msg=`git log -1 --pretty=%B | tr -s ' ' | tr ' ' '_'`
 
 systemctl restart irrigation_7542.service
 systemctl restart rules_handler.service
-systemctl restart irrigation_viber_bot.service
+#systemctl restart irrigation_viber_bot.service
 systemctl restart irrigation_power_supply.service
 
 echo 'HEAD is now '$msg
