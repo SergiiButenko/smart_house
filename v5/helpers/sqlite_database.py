3d85ed17644bc82476ff3e5410819647589b8604
Sergii Butenko <sergii.butenko@globallogic.com>
2017-10-26 18:32:09 +0300

    fixed code

--- v5/helpers/sqlite_database.py
+++ v5/helpers/sqlite_database.py
@@ -4,29 +4,83 @@ from helpers.redis import *
 from helpers.common import *
 
 QUERY = {}
-QUERY['get_next_active_rule'] = "SELECT l.id, l.line_id, l.rule_id, l.timer as \"[timestamp]\", l.interval_id, l.time, li.name FROM life AS l, lines as li WHERE l.state = 1 AND l.active=1 AND l.line_id={0} AND li.number = l.line_id AND timer>=datetime('now', 'localtime') ORDER BY timer LIMIT 1"
-QUERY['get_last_start_rule'] = "SELECT l.id, l.line_id, l.rule_id, l.timer as \"[timestamp]\", l.interval_id  FROM life AS l WHERE l.state = 2 AND l.active=1 AND l.rule_id = 1 AND l.line_id={0} AND timer<=datetime('now', 'localtime') ORDER BY timer DESC LIMIT 1"
-QUERY['get_table_body_only'] = "SELECT l.id, li.name, rule_type.name, l.state, l.date, l.timer as \"[timestamp]\", l.active, rule_state.full_name, l.interval_id FROM life as l, type_of_rule as rule_type, lines as li, state_of_rule as rule_state WHERE l.rule_id = rule_type.id AND l.line_id = li.number and l.state = rule_state.id order by l.id, l.timer desc, l.interval_id"
+QUERY['get_next_active_rule'] = (
+    "SELECT l.id, l.line_id, l.rule_id, l.timer as \"[timestamp]\", l.interval_id, l.time, li.name "
+    "FROM life AS l, lines as li "
+    "WHERE l.state = 1 AND l.active=1 AND l.line_id={0} AND li.number = l.line_id AND timer>=datetime('now', 'localtime') "
+    "ORDER BY timer LIMIT 1")
 
-QUERY['ongoing_rules_table'] = "SELECT w.id, dw.name, li.name, rule_type.name, \"time\" as \"[timestamp]\", \"interval\", w.active FROM week_schedule as w, day_of_week as dw, lines as li, type_of_rule as rule_type WHERE  w.day_number = dw.num AND w.line_id = li.number and w.rule_id = rule_type.id ORDER BY w.day_number, w.time"
+QUERY['get_last_start_rule'] = (
+    "SELECT l.id, l.line_id, l.rule_id, l.timer as \"[timestamp]\", l.interval_id "
+    "FROM life AS l "
+    "WHERE l.state = 2 AND l.active=1 AND l.rule_id = 1 AND l.line_id={0} AND timer<=datetime('now', 'localtime') "
+    "ORDER BY timer DESC LIMIT 1")
 
-QUERY['history'] = "SELECT l.id, li.name, rule_type.name, l.state, l.date, l.timer as \"[timestamp]\", l.active, rule_state.full_name, l.time FROM life as l, type_of_rule as rule_type, lines as li, state_of_rule as rule_state WHERE l.rule_id = rule_type.id AND l.line_id = li.number AND l.timer >= datetime('now', 'localtime', '-{0} day') and l.state = rule_state.id order by l.timer desc"
+QUERY['get_table_body_only'] = (
+    "SELECT l.id, li.name, rule_type.name, l.state, l.date, l.timer as \"[timestamp]\", l.active, rule_state.full_name, l.interval_id "
+    "FROM life as l, type_of_rule as rule_type, lines as li, state_of_rule as rule_state "
+    "WHERE l.rule_id = rule_type.id AND l.line_id = li.number AND l.state = rule_state.id "
+    "order by l.id, l.timer desc, l.interval_id")
 
-QUERY['ongoing_rules'] = "SELECT w.id, dw.name, li.name, rule_type.name, \"time\" as \"[timestamp]\", \"interval\", w.active FROM week_schedule as w, day_of_week as dw, lines as li, type_of_rule as rule_type WHERE  w.day_number = dw.num AND w.line_id = li.number and w.rule_id = rule_type.id ORDER BY w.day_number, w.time"
+QUERY['ongoing_rules_table'] = (
+    "SELECT w.id, dw.name, li.name, rule_type.name, \"time\" as \"[timestamp]\", \"interval\", w.active "
+    "FROM week_schedule as w, day_of_week as dw, lines as li, type_of_rule as rule_type "
+    "WHERE  w.day_number = dw.num AND w.line_id = li.number AND w.rule_id = rule_type.id "
+    "ORDER BY w.day_number, w.time")
 
-QUERY['get_timetable_list_1'] = "SELECT l.id, li.name, rule_type.name, l.state, l.date, l.timer as \"[timestamp]\", l.active, rule_state.full_name, l.time FROM life as l, type_of_rule as rule_type, lines as li, state_of_rule as rule_state WHERE l.rule_id = rule_type.id AND l.line_id = li.number AND l.timer<=datetime('now', 'localtime','+{0} day') and l.state = rule_state.id  order by l.timer desc"
-QUERY['get_timetable_list_2'] = "SELECT l.id, li.name, rule_type.name, l.state, l.date, l.timer as \"[timestamp]\", l.active, rule_state.full_name, l.time FROM life as l, type_of_rule as rule_type, lines as li, state_of_rule as rule_state WHERE l.rule_id = rule_type.id AND l.line_id = li.number AND l.timer>= datetime('now', 'localtime', '-{0} hour') and l.timer<=datetime('now', 'localtime', '+{0} hour') and l.state = rule_state.id  order by l.timer desc"
+QUERY['history'] = (
+    "SELECT l.id, li.name, rule_type.name, l.state, l.date, l.timer as \"[timestamp]\", l.active, rule_state.full_name, l.time "
+    "FROM life as l, type_of_rule as rule_type, lines as li, state_of_rule as rule_state "
+    "WHERE l.rule_id = rule_type.id AND l.line_id = li.number AND l.timer >= datetime('now', 'localtime', '-{0} day') AND l.state = rule_state.id "
+    "order by l.timer desc")
 
-QUERY['add_rule'] = "INSERT INTO life(line_id, rule_id, state, date, timer, interval_id, time) VALUES ({0}, {1}, {2}, '{3}', '{4}', '{5}', {6})"
-QUERY['add_rule_endpoint_v2'] = "INSERT INTO life(line_id, rule_id, state, date, timer, interval_id, time) VALUES ({0}, {1}, {2}, '{3}', '{4}', '{5}', {6})"
+QUERY['ongoing_rules'] = (
+    "SELECT w.id, dw.name, li.name, rule_type.name, \"time\" as \"[timestamp]\", \"interval\", w.active "
+    "FROM week_schedule as w, day_of_week as dw, lines as li, type_of_rule as rule_type "
+    "WHERE w.day_number = dw.num AND w.line_id = li.number AND w.rule_id = rule_type.id "
+    "ORDER BY w.day_number, w.time")
 
-QUERY['add_ongoing_rule'] = "INSERT INTO week_schedule(day_number, line_id, rule_id, \"time\", \"interval\", active) VALUES ({0}, {1}, {2}, '{3}', {4}, 1)"
+QUERY['get_timetable_list_1'] = (
+    "SELECT l.id, li.name, rule_type.name, l.state, l.date, l.timer as \"[timestamp]\", l.active, rule_state.full_name, l.time "
+    "FROM life as l, type_of_rule as rule_type, lines as li, state_of_rule as rule_state "
+    "WHERE l.rule_id = rule_type.id AND l.line_id = li.number AND l.timer<=datetime('now', 'localtime','+{0} day') AND l.state = rule_state.id "
+    "ORDER BY l.timer desc")
 
-QUERY['activate_branch_1'] = "INSERT INTO life(line_id, rule_id, state, date, timer, interval_id, time) VALUES ({0}, {1}, {2}, '{3}', '{4}', '{5}', {6})"
-QUERY['activate_branch_2'] = "SELECT l.id, l.line_id, l.rule_id, l.timer, l.interval_id, l.time, li.name FROM life as l, lines as li where l.id = {0} and li.number = l.line_id"
+QUERY['get_timetable_list_2'] = (
+    "SELECT l.id, li.name, rule_type.name, l.state, l.date, l.timer as \"[timestamp]\", l.active, rule_state.full_name, l.time "
+    "FROM life as l, type_of_rule as rule_type, lines as li, state_of_rule as rule_state "
+    "WHERE l.rule_id = rule_type.id AND l.line_id = li.number AND l.timer>= datetime('now', 'localtime', '-{0} hour') AND l.timer<=datetime('now', 'localtime', '+{0} hour') AND l.state = rule_state.id "
+    "ORDER BY l.timer desc")
 
-QUERY['deactivate_branch_1'] = "UPDATE life SET state=4 WHERE interval_id = '{0}' and state = 1"
-QUERY['deactivate_branch_2'] = "INSERT INTO life(line_id, rule_id, state, date, timer, interval_id) VALUES ({0}, {1}, {2}, '{3}', '{4}', '{5}')"
+QUERY['add_rule'] = (
+    "INSERT INTO life(line_id, rule_id, state, date, timer, interval_id, time) "
+    "VALUES ({0}, {1}, {2}, '{3}', '{4}', '{5}', {6})")
+
+QUERY['add_rule_endpoint_v2'] = (
+    "INSERT INTO life(line_id, rule_id, state, date, timer, interval_id, time) "
+    "VALUES ({0}, {1}, {2}, '{3}', '{4}', '{5}', {6})")
+
+QUERY['add_ongoing_rule'] = (
+    "INSERT INTO week_schedule(day_number, line_id, rule_id, \"time\", \"interval\", active) "
+    "VALUES ({0}, {1}, {2}, '{3}', {4}, 1)")
+
+QUERY['activate_branch_1'] = (
+    "INSERT INTO life(line_id, rule_id, state, date, timer, interval_id, time) "
+    "VALUES ({0}, {1}, {2}, '{3}', '{4}', '{5}', {6})")
+
+QUERY['activate_branch_2'] = (
+    "SELECT l.id, l.line_id, l.rule_id, l.timer, l.interval_id, l.time, li.name "
+    "FROM life as l, lines as li "
+    "WHERE l.id = {0} AND li.number = l.line_id")
+
+QUERY['deactivate_branch_1'] = (
+    "UPDATE life "
+    "SET state=4 "
+    "WHERE interval_id = '{0}' AND state = 1")
+
+QUERY['deactivate_branch_2'] = (
+    "INSERT INTO life(line_id, rule_id, state, date, timer, interval_id) "
+    "VALUES ({0}, {1}, {2}, '{3}', '{4}', '{5}')"
 
 QUERY['enable_rule'] = "UPDATE life SET state=2 WHERE id={0}"
 QUERY['enable_rule_state_6'] = "UPDATE life SET state=6 WHERE id={0}"
@@ -42,14 +96,16 @@ QUERY['remove_ongoing_rule'] = "DELETE from week_schedule WHERE id={0}"
 QUERY['edit_ongoing_rule'] = "DELETE from week_schedule WHERE id={0}"
 
 QUERY['cancel_rule_1'] = "SELECT l.interval_id, li.name FROM life AS l, lines AS li WHERE l.id = {0} AND l.line_id = li.number"
-QUERY['cancel_rule_2'] = "UPDATE life SET state=4 WHERE interval_id = '{0}' and state = 1"
+QUERY['cancel_rule_2'] = "UPDATE life SET state=4 WHERE interval_id = '{0}' AND state = 1"
 
 QUERY['temperature_1'] = "SELECT * FROM temperature_statistics limit 1"
-QUERY['temperature_2'] = "INSERT INTO temperature_statistics (temperature_street, humidity_street, temperature_small_h_1_fl, humidity_small_h_1_fl, temperature_small_h_2_fl, humidity_small_h_2_fl, temperature_big_h_1_fl, humidity_big_h_1_fl, temperature_big_h_2_fl, humidity_big_h_2_fl) VALUES ('{0}', '{1}', '{2}', '{3}', '{4}', '{5}', '{6}', '{7}', '{8}', '{9}')"
+QUERY['temperature_2'] = (
+    "INSERT INTO temperature_statistics (temperature_street, humidity_street, temperature_small_h_1_fl, humidity_small_h_1_fl, temperature_small_h_2_fl, humidity_small_h_2_fl, temperature_big_h_1_fl, humidity_big_h_1_fl, temperature_big_h_2_fl, humidity_big_h_2_fl) "
+    "VALUES ('{0}', '{1}', '{2}', '{3}', '{4}', '{5}', '{6}', '{7}', '{8}', '{9}')")
 
-QUERY['get_settings'] = "SELECT number, name, time, intervals, time_wait, start_time, line_type, base_url, pump_enabled from lines order by number"
+QUERY['get_settings'] = "SELECT number, name, time, intervals, time_wait, start_time, line_type, base_url, pump_enabled FROM lines ORDER BY number"
 
-QUERY['enable_rule_cancel_interval'] = "UPDATE life SET state={1} WHERE state=1 and interval_id={0}"
+QUERY['enable_rule_cancel_interval'] = "UPDATE life SET state={1} WHERE state=1 AND interval_id={0}"
 
 
 # executes query and returns fetch* result
