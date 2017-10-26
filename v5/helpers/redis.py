0f67650bdea14a926f9331d481b2f769f26859ea
Sergii Butenko <sergii.butenko@globallogic.com>
2017-10-26 15:38:51 +0300

    fixed code

--- v5/helpers/redis.py
+++ v5/helpers/redis.py
@@ -2,6 +2,8 @@ import redis
 import logging
 import json
 import datetime
+from dateutil import parser
+
 from pytz import timezone
 from helpers.common import *
 
@@ -40,7 +42,7 @@ def date_hook(json_dict):
             pass
 
         try:
-            json_dict[key] = datetime.datetime.strptime(value, "%Y-%m-%dT%H:%M:%S%z")
+            json_dict[key] = parser.parse(value)
         except:
             pass
             # 2017-10-26T15:29:51.685474+00:00
