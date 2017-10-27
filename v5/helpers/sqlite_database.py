a5bc53329fc6ea421a22925b50fdccab32e7cb3f
Sergii Butenko <sergii.butenko@globallogic.com>
2017-10-27 17:54:59 +0300

    Changed flow

--- v5/helpers/sqlite_database.py
+++ v5/helpers/sqlite_database.py
@@ -96,7 +96,7 @@ QUERY['remove_ongoing_rule'] = "DELETE from week_schedule WHERE id={0}"
 QUERY['edit_ongoing_rule'] = "DELETE from week_schedule WHERE id={0}"
 
 QUERY['cancel_rule_1'] = "SELECT l.interval_id, li.name FROM life AS l, lines AS li WHERE l.id = {0} AND l.line_id = li.number"
-QUERY['cancel_rule_2'] = "UPDATE life SET state=4 WHERE interval_id = '{0}' AND state = 1"
+QUERY['cancel_rule_2'] = "UPDATE life SET state = 4 WHERE interval_id = '{0}' AND state = 1"
 
 QUERY['temperature_1'] = "SELECT * FROM temperature_statistics limit 1"
 QUERY['temperature_2'] = (
