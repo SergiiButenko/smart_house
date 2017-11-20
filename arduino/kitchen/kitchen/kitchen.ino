6097b43e08088b09256a100e073fff44ea4b7527
sergii <butenko2003@ukr.net>
2017-11-20 10:16:29 +0200

    kitche

--- arduino/kitchen/kitchen/kitchen.ino
+++ arduino/kitchen/kitchen/kitchen.ino
@@ -1,26 +1,30 @@
-int relay_pin = 4;
+int relay_pin = 12;
 int ir_pin=2;
 volatile byte toogle_on_off = 0;
 
 void setup()  {
 
- Serial.begin(9600); //Start serial communication boud rate at 9600
- pinMode(ir_pin,INPUT); //Pin 5 as signal input
- pinMode(relay_pin, OUTPUT);
- 
- attachInterrupt(1, toogle, FALLING);
+  Serial.begin(9600); //Start serial communication boud rate at 9600
+  pinMode(ir_pin,INPUT_PULLUP); //Pin 5 as signal input
+  pinMode(relay_pin, OUTPUT);
+  digitalWrite(relay_pin, LOW);
+
+  attachInterrupt(0, toogle, RISING);
 
 }
 void loop()  {
   if (toogle_on_off == 1){
-   delay(500);
-   digitalWrite(relay_pin, !digitalRead(relay_pin));
-  toogle_on_off = 0;
+    Serial.println("on");
+    digitalWrite(relay_pin, !digitalRead(relay_pin));
+    toogle_on_off = 0;
+    delay(1000);
+    toogle_on_off = 0;
   }
 }
 
 void toogle() {
   toogle_on_off = 1;
 }
+
 
 
