1f94d1513afe72491bd173afe7b6b96b5ec6cbd2
sergii <butenko2003@ukr.net>
2017-11-26 20:51:25 +0200

    big kitchen

--- arduino/kitchen/kitchen/kitchen.ino
+++ arduino/kitchen/kitchen/kitchen.ino
@@ -1,12 +1,14 @@
 int relay_pin = 12;
 int ir_pin=2;
 volatile byte toogle_on_off = 0;
-volatile int delta = 50;
+volatile int delta = 70;
 
 void setup()  {
 
   Serial.begin(9600); //Start serial communication boud rate at 9600
-  //  pinMode(ir_pin,INPUT); //Pin 5 as signal input
+  pinMode(ir_pin,INPUT); //Pin 5 as signal input
+  pinMode(A0,INPUT); //Pin 5 as signal input
+
   pinMode(relay_pin, OUTPUT);
   digitalWrite(relay_pin, HIGH);
 
@@ -14,10 +16,13 @@ void setup()  {
 
 }
 void loop()  {
+  // Serial.print(digitalRead(ir_pin));
+  // Serial.println(digitalRead(A0));
+
   if (toogle_on_off == 1){
     delay(delta);
 
-    if (digitalRead(ir_pin) == HIGH){
+    if ((digitalRead(ir_pin) == HIGH) || (digitalRead(A0) == HIGH)) {
       toogle_on_off = 0;
       return;
     }
@@ -29,10 +34,11 @@ void loop()  {
 }
 
 void toogle() {
-  if (digitalRead(ir_pin) == LOW){
+  if ((digitalRead(ir_pin) == LOW) && (digitalRead(A0) == LOW)){
     toogle_on_off = 1;
   }
 }
+
 
 
 
