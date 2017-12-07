2bdc0a64488e3b9ce63e79f4986d0af101ebba7c
Sergii Butenko <sergii.butenko@globallogic.com>
2017-12-07 10:36:52 +0200

    files changed

--- tools/scripts/restart_7543.sh
+++
@@ -1,9 +0,0 @@
-cd /var/www/
-git reset --hard
-git pull
-msg=`git log -1 --pretty=%B | tr -s ' ' | tr ' ' '_'`
-
-systemctl restart irrigation_7543.service
-systemctl restart rules_handler.service
-
-echo 'HEAD is now '$msg
--- v5/helpers/common.py
+++
@@ -1,4 +0,0 @@
-import inspect
-
-# For get function name intro function. Usage mn(). Return string with current function name. Instead 'query' will be database.QUERY[mn()].format(....)
-mn = lambda: inspect.stack()[1][3]
