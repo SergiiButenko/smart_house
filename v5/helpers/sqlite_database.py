383fa4d8b60d8d8a691689f305381c8ca735eb8b
sergii <butenko2003@ukr.net>
2017-12-09 20:21:28 +0200

    changes for ui

--- v5/helpers/sqlite_database.py
+++ v5/helpers/sqlite_database.py
@@ -56,7 +56,7 @@ QUERY['ongoing_rules'] = (
 QUERY['add_ongoing_rule'] = (
     "INSERT INTO life(line_id, time, intervals, time_wait, repeat_value, date_start, "
     "time_start, end_date, active, rule_id) "
-    "VALUES ({0}, {1}, {2}, {3}, {4}, {5}, '{6}', '{7}', {8}, {9}, {10}, {11}, '{12}')")
+    "VALUES ({0}, {1}, {2}, {3}, {4}, '{5}', '{6}', '{7}', {8}, '{9}')")
 
 QUERY['add_rule'] = (
     "INSERT INTO life(line_id, rule_id, state, date, timer, interval_id, time) "
