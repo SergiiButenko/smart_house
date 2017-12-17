07e194382c9b25339720878ea79ddbd23552627c
sergii <butenko2003@ukr.net>
2017-12-18 01:43:26 +0200

    html

--- web/irrigation_web.py
+++ web/irrigation_web.py
@@ -21,7 +21,7 @@ from helpers.common import *
 
 eventlet.monkey_patch()
 logging.basicConfig(format='%(asctime)s - %(levelname)s - %(filename)s:%(lineno)d - %(message)s',
-                    datefmt='%m/%d/%Y %I:%M:%S %p', level=logging.INFO)
+                    datefmt='%m/%d/%Y %I:%M:%S %p', level=logging.DEBUG)
 
 logging.getLogger('socketio').setLevel(logging.ERROR)
 logging.getLogger('engineio').setLevel(logging.ERROR)
