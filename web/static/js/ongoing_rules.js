53805bd4169f52ba7572c04fdc571e603a5d5250
sergii <butenko@linux>
2017-12-25 15:03:53 +0200

    Ongoing rules

--- web/static/js/ongoing_rules.js
+++ web/static/js/ongoing_rules.js
@@ -209,7 +209,7 @@ function form_text(el_in) {
 
     $(card).find("#summary").innerHTML(
         schedule_text + ' о ' + time + ', ' + text + '.</br>' +
-        intervals + ' рази, по ' + minutes + ' хвилин'
+        interval + ' рази, по ' + minutes + ' хвилин'
         );
 }
 
