581fb8fe4a435b48cc9b52a60ee414e2aad179ca
sergii <butenko2003@ukr.net>
2018-01-27 14:21:37 +0200

    suples

--- web/irrigation_web.py
+++ web/irrigation_web.py
@@ -269,14 +269,14 @@ def history():
             row = intervals[0]
             id = row[0]
 
-            rules.append(list('line_name', row[1]),
+            rules.append(list(('line_name', row[1]),
                                 ('date', row[2].strftime('%m/%d/%Y')),
                                 ('timer', date_handler(row[3])),
                                 ('ative', row[4]),
                                 ('time', row[5]),
                                 ('intervals', interval),
                                 ('interval_id', row[0]),
-                                ('time_wait', 15))
+                                ('time_wait', 15)))
     return jsonify(rules=rules)
     return render_template('history.html', my_list=rules)
 
