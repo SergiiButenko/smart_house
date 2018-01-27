61146cf39f80bb4ddc479605d9cadb32e7ebcf94
sergii <butenko2003@ukr.net>
2018-01-27 10:37:17 +0200

    LIST

--- web/irrigation_web.py
+++ web/irrigation_web.py
@@ -269,14 +269,16 @@ def history():
             row = intervals[0]
             id = row[0]
 
-            rules.append(dict(branch_name=row[1],
+            rules.append(dict(line_name=row[1],
                                 date=row[2].strftime('%m/%d/%Y'),
                                 timer=date_handler(row[3]),
                                 ative=row[4],
                                 time=row[5],
-                                intervals=interval))
+                                intervals=interval,
+                                interval_id=row[0]),
+                                time_wait=15)
 
-    return jsonify(rules=rules)
+    return render_template('history.html', my_list=rules)
 
 
 # @app.route("/add_rule", methods=['POST'])
