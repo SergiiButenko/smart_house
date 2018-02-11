13532fb2b540b2958f2923560fbb4571719cf01d
sergii <butenko2003@ukr.net>
2018-02-12 01:44:04 +0200

    modified chartds

--- web/static/js/index_min.js
+++ web/static/js/index_min.js
@@ -351,25 +351,30 @@ function draw_d3js(id, data) {
 
     // 5. X scale will use the index of our data
     var xScale = d3.scaleLinear()
-        .domain([0, n]) // input
+        .domain([0, n - 1]) // input
         .range([0, width]); // output
 
     // 6. Y scale will use the randomly generate number 
     var yScale = d3.scaleLinear()
-        .domain([0, 100]) // input 
+        .domain([0, 1]) // input 
         .range([height, 0]); // output 
 
     // 7. d3's line generator
     var line = d3.line()
-        .x(function(d) { return xScale(d['hours']); }) // set the x values for the line generator
-        .y(function(d) { return yScale(d['val']); }) // set the y values for the line generator 
+        .x(function(d, i) { return xScale(i); }) // set the x values for the line generator
+        .y(function(d) { return yScale(d.y); }) // set the y values for the line generator 
         .curve(d3.curveMonotoneX) // apply smoothing to the line
 
+
     // 8. An array of objects of length N. Each object has key -> value pair, the key being "y" and the value is a random number
+    // 8. An array of objects of length N. Each object has key -> value pair, the key being "y" and the value is a random number
+    
+
     var dataset = data
     var dataset2 = Array(n).fill({ 'base_val': 24, 'hours': 0 })
+    var dataset = d3.range(n).map(function(d) { return { "y": d3.randomUniform(1)() } })
     //d3.range(n).map(function(d) { return {"y": d3.randomUniform(1)() } })
-    
+
     // 1. Add the SVG to the page and employ #2
     var svg = d3.select(`#card-${id} chart`).append("svg")
         .attr("width", width + margin.left + margin.right)
@@ -390,22 +395,16 @@ function draw_d3js(id, data) {
 
     // 9. Append the path, bind the data, and call the line generator 
     svg.append("path")
-        .datum(dataset2) // 10. Binds data to the line 
-        .attr("class", "line_base") // Assign a class for styling 
-        .attr("d", line); // 11. Calls the line generator 
-
-    // 9. Append the path, bind the data, and call the line generator 
-    svg.append("path")
         .datum(dataset) // 10. Binds data to the line 
         .attr("class", "line") // Assign a class for styling 
         .attr("d", line); // 11. Calls the line generator 
 
     // 12. Appends a circle for each datapoint 
-    // svg.selectAll(".line > .dot")
-    //     .data(dataset)
-    //     .enter().append("circle") // Uses the enter().append() method
-    //     .attr("class", "dot") // Assign a class for styling
-    //     .attr("cx", function(d, i) { return xScale(d) })
-    //     .attr("cy", function(d) { return yScale(d) })
-    //     .attr("r", 5);
+    svg.selectAll(".dot")
+        .data(dataset)
+        .enter().append("circle") // Uses the enter().append() method
+        .attr("class", "dot") // Assign a class for styling
+        .attr("cx", function(d, i) { return xScale(i) })
+        .attr("cy", function(d) { return yScale(d.y) })
+        .attr("r", 5);
 }
