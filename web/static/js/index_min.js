49e18b8c437968b301ca8699bbb936e01bac9674
sergii <butenko2003@ukr.net>
2018-02-12 01:35:09 +0200

    modified chartds

--- web/static/js/index_min.js
+++ web/static/js/index_min.js
@@ -351,18 +351,18 @@ function draw_d3js(id, data) {
 
     // 5. X scale will use the index of our data
     var xScale = d3.scaleLinear()
-        //.domain([0, n]) // input
+        .domain([0, n]) // input
         .range([0, width]); // output
 
     // 6. Y scale will use the randomly generate number 
     var yScale = d3.scaleLinear()
-        //.domain([0, 100]) // input 
+        .domain([0, 100]) // input 
         .range([height, 0]); // output 
 
     // 7. d3's line generator
     var line = d3.line()
-        .x(function(d, i) { return xScale(i.hours); }) // set the x values for the line generator
-        .y(function(d) { return yScale(d.val); }) // set the y values for the line generator 
+        .x(function(d) { return xScale(d['hours']); }) // set the x values for the line generator
+        .y(function(d) { return yScale(d['val'];); }) // set the y values for the line generator 
         .curve(d3.curveMonotoneX) // apply smoothing to the line
 
     // 8. An array of objects of length N. Each object has key -> value pair, the key being "y" and the value is a random number
@@ -390,13 +390,13 @@ function draw_d3js(id, data) {
 
     // 9. Append the path, bind the data, and call the line generator 
     svg.append("path")
-        .datum([dataset2]) // 10. Binds data to the line 
+        .datum(dataset2) // 10. Binds data to the line 
         .attr("class", "line_base") // Assign a class for styling 
         .attr("d", line); // 11. Calls the line generator 
 
     // 9. Append the path, bind the data, and call the line generator 
     svg.append("path")
-        .datum([dataset]) // 10. Binds data to the line 
+        .datum(dataset) // 10. Binds data to the line 
         .attr("class", "line") // Assign a class for styling 
         .attr("d", line); // 11. Calls the line generator 
 
