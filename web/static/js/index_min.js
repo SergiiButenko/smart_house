55f5f5050f03c85c923d56439695f506b4582d7d
sergii <butenko2003@ukr.net>
2018-02-12 02:46:56 +0200

    modified chartds

--- web/static/js/index_min.js
+++ web/static/js/index_min.js
@@ -375,7 +375,8 @@ function draw_d3js(id, data) {
 
     var dataset2 = [] //Array(n).fill({ 'base_val': data['base']})
     for (var i = 0; i <= dataset.length; i++) {    
-        dataset2[i] = {'base_val': data['base'], 'hours': data['new'][i].hours}
+        console.log(data['new'][i])
+        dataset2[i] = {'base_val': data['base'], 'hours': data['new'][i]}
     }
     console.log(dataset2);
     xScale.domain([d3.min(dataset, function(d) { return d.hours; }), d3.max(dataset, function(d) { return d.hours; })]);
