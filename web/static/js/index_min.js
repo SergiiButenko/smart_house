f25a2fe53c57e3c0a9502807bd266c07f82a5013
sergii <butenko2003@ukr.net>
2018-02-12 03:05:20 +0200

    modified chartds

--- web/static/js/index_min.js
+++ web/static/js/index_min.js
@@ -378,7 +378,7 @@ function draw_d3js(id, data) {
         dataset2[i] = { 'base_val': data['base'], 'hours': data['new'][i]['hours'] }
         hours.push(data['new'][i]['hours']);
     }
-    xScale.domain(hours);
+    xScale.domain([hours]);
 
     
     //var dataset = d3.range(n).map(function(d) { return { "y": d3.randomUniform(1)() } })
