908bd9dced491da0d92fb1aa0863c624cc11ef20
sergii <butenko2003@ukr.net>
2018-02-12 03:04:34 +0200

    modified chartds

--- web/static/js/index_min.js
+++ web/static/js/index_min.js
@@ -372,13 +372,15 @@ function draw_d3js(id, data) {
     // 8. An array of objects of length N. Each object has key -> value pair, the key being "y" and the value is a random number
     // 8. An array of objects of length N. Each object has key -> value pair, the key being "y" and the value is a random number
     var dataset = data['new']
-
     var dataset2 = [] //Array(n).fill({ 'base_val': data['base']})
+    var hours = []
     for (var i = 0; i < dataset.length; i++) {
         dataset2[i] = { 'base_val': data['base'], 'hours': data['new'][i]['hours'] }
+        hours.push(data['new'][i]['hours']);
     }
+    xScale.domain(hours);
 
-    xScale.domain([d3.min(dataset, function(d) { return d.hours; }), d3.max(dataset, function(d) { return d.hours; })]);
+    
     //var dataset = d3.range(n).map(function(d) { return { "y": d3.randomUniform(1)() } })
     //d3.range(n).map(function(d) { return {"y": d3.randomUniform(1)() } })
 
